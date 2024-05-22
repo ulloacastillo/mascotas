ea26ed56cc7ea2283e138a7c793bde0e1b5f3577
ulloacastillo <jorgeulloacastillo@gmail.com>
2024-05-22 12:19:05 -0400

    p3974: join controllers inside PetController

--- src/AppBundle/Controller/PetController.php
+++ src/AppBundle/Controller/PetController.php
@@ -2,22 +2,45 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use AppBundle\Entity\Mascota;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-
-class IngresarController extends Controller
+/**
+ * @Route("/mascotas", name="mascota.")
+ */
+class PetController extends Controller
 {
     /**
-     * @Route("/mascotas/ingresar", name="ingresarMascotas")
+     * @Route("/listar", name="listarMascotas")
      */
-    public function mascotasAction(Request $request)
+    public function showAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $query = $entityManager->createQuery(
+            "SELECT m
+            FROM AppBundle:Mascota m"
+        );
+        $results = $query->getResult();
+        dump($results);
+
+
+
+        return $this->render("/mascotas/listar.html.twig", [
+            'pets' => $results
+        ]);
+    }
+
+    /**
+     * @Route("/ingresar", name="ingresarMascotas")
+     */
+    public function insertAction(Request $request)
     {
         $mascota = new Mascota();
 
@@ -69,4 +92,14 @@ class IngresarController extends Controller
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/modificar", name="modificarMascotas")
+     */
+    public function modifyAction(Request $request)
+    {
+
+        return $this->render("/mascotas/modificar.html.twig");
+    }
 }
+
